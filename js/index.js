8035cd5e26fe8246e3c118ac12a68233fccdd89d
DESKTOP-TO2B1U4\user <ryoryo8haruharu@gmail.com>
2025-10-14 02:24:42 +0900

    commit

--- js/index.js
+++ js/index.js
@@ -1,14 +1,18 @@
 $(function () {
   let index = 0;
   const $images = $(".slideshow img");
+
+  // 最初の画像にもアニメーションがかかる
   $images.eq(index).addClass("active");
 
   setInterval(() => {
-    // 今の画像を消す
+    // 現在の画像を非表示にする
     $images.eq(index).removeClass("active");
 
-    // 次の画像へ
+    // 次の画像に進む
     index = (index + 1) % $images.length;
+
+    // 次の画像に active を付ける（アニメーション開始）
     $images.eq(index).addClass("active");
-  }, 5000); // 5秒ごとに切り替え
+  }, 5000);
 });
