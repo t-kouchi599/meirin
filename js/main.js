781792d42bbec5af492590baf246c74b61ba2054
DESKTOP-TO2B1U4\user <ryoryo8haruharu@gmail.com>
2025-10-16 10:25:44 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -62,5 +62,38 @@ $(function() {
   $(window).resize(function() {
     setGridColumns();
   });
+
+  // 音楽再生
+  const bgm = $("#bgm")[0];
+
+  // 前回の再生秒数を復元
+  const savedTime = sessionStorage.getItem("bgmTime");
+  if (savedTime) {
+    bgm.currentTime = parseFloat(savedTime);
+  }
+
+  // 再生状態を復元
+  if (sessionStorage.getItem("bgmPlaying") === "true") {
+    bgm.play();
+    $("#music-play-btn").text("■");
+  }
+
+  // 再生/停止ボタン
+  $("#music-play-btn").on("click", function() {
+    if (bgm.paused) {
+      bgm.play();
+      $(this).text("■");
+      sessionStorage.setItem("bgmPlaying", "true");
+    } else {
+      bgm.pause();
+      $(this).text("♪");
+      sessionStorage.setItem("bgmPlaying", "false");
+    }
+  });
+
+  // 定期的に現在の再生秒数を保存
+  bgm.addEventListener("timeupdate", function() {
+    sessionStorage.setItem("bgmTime", bgm.currentTime);
+  });
 });
 
